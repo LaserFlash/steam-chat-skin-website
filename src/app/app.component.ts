000b5d0caae553bef588bc5590965fbbe8314c46
Bryn Bennett <bryn.t.bennett@gmail.com>
2019-08-11 20:05:52 +1200

    Hide top toolbar untill scroll down

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HostListener, Inject } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'steam-chat-skin-website';
+  title = "steam-chat-skin-website";
+  screenHeight: number;
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    this.screenHeight = window.innerHeight;
+    const number =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+    if (number == 0) {
+      document.querySelector("body").style.cssText = "--visibility: hidden";
+    } else if (number > this.screenHeight * 0.3) {
+      document.querySelector("body").style.cssText =
+        "--visibility: visible";
+    }
+    console.log("scroll" + number)
+    console.log("height" +  this.screenHeight)
+  }
 }
