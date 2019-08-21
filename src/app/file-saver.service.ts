e6ae6f68c63c979964a2fbab95a049205b7a89a0
Bryn Bennett <bryn.t.bennett@gmail.com>
2019-08-22 09:56:20 +1200

    Use older filesaver.js version to stop popup detection.
    Issue identified here: https://github.com/eligrey/FileSaver.js/issues/561

--- src/app/file-saver.service.ts
+++ src/app/file-saver.service.ts
@@ -8,10 +8,7 @@ export class FileSaverService {
   constructor() {}
 
   createAndSaveFromArray(lines: string[], fileName: string) {
-    const blob = new Blob(lines, {
-      type: "text/css;charset=utf-8",
-      endings: "native"
-    });
+    const blob = new File(lines, fileName, {type: "text/plain;charset=utf-8"});
     FileSaver.saveAs(blob, fileName);
   }
 }
