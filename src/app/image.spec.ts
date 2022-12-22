92f5d3b3bac0dd12150faa8d7d40cec2f8cd9df9
Bryn Bennett <bryn.t.bennett@gmail.com>
2022-12-22 01:14:18 +0000

    TECH: remove testing dependencies (we don't have any tests)

--- src/app/image.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Image } from './image';
-
-describe('Image', () => {
-  it('should create an instance', () => {
-    expect(new Image()).toBeTruthy();
-  });
-});
--- src/app/steam-chat-style-options.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SteamChatStyleOption } from './steam-chat-style-options';
-
-describe('SteamChatStyleOptions', () => {
-  it('should create an instance', () => {
-    expect(new SteamChatStyleOption()).toBeTruthy();
-  });
-});
